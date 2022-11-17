2646cf10a13825b329656d7871987f066c6d2d27
Olha Fastiuk <ofastiuk@tsukat.com>
2022-11-17 19:30:56 +0200

    Add technologies block

---
+++ src/components/technologies/Technologies.js
@@ -0,0 +1,80 @@
+import './technologies.css';
+
+export default function Technologies() {
+    return (
+        <div id="technologiesWrapper">
+            <h1>the stack we provide</h1>
+            <div className="vrLine"></div>
+            <div className="technologiesBlock">
+                <h3>Design</h3>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-8.jpg')} />
+
+                    <span>figma</span>
+                </div>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-9.jpg')} />
+                    <span>photoshop</span>
+                </div>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-11.jpg')} />
+
+                    <span>illustrator</span>
+                </div>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-12.jpg')} />
+
+                    <span>indesign</span>
+                </div>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-10.jpg')} />
+
+                    <span>xd</span>
+                </div>
+            </div>
+            <div className="vrLine"></div>
+            <div className="technologiesBlock">
+                <h3>Front-end</h3>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon.jpg')} />
+                    <span>angular</span>
+                </div>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-1.jpg')} />
+                    <span>react</span>
+                </div>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-2.jpg')} />
+                    <span>next. js</span>
+                </div>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-3.jpg')} />
+                    <span>vue</span>
+                </div>
+            </div>
+            <div className="vrLine"></div>
+            <div className="technologiesBlock">
+                <h3>Back-end</h3>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-4.jpg')} />
+                    <span>node</span>
+                </div>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-5.jpg')} />
+
+                    <span>python</span>
+                </div>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-6.jpg')} />
+
+                    <span>rails</span>
+                </div>
+                <div className="technology">
+                    <img src={require('../images/technologies/Icon-7.jpg')} />
+
+                    <span>django</span>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -3,6 +3,7 @@ import Header from '../components/header/Header';
 import HeroBlock from '../components/heroBlock/HeroBlock';
 import Projects from '../components/projects/Projects';
 import Services from '../components/services/Services';
+import Technologies from '../components/technologies/Technologies';
 import './homepage.css';
 
 export default function Homepage() {
@@ -13,6 +14,7 @@ export default function Homepage() {
             <AboutUs />
             <Services />
             <Projects />
+            <Technologies />
         </>
     );
 }
