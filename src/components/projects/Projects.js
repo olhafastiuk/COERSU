4b6783c29cb4860e3207f320c7a05878154ce587
Olha Fastiuk <ofastiuk@tsukat.com>
2022-12-06 21:28:43 +0200

    Add slider to projects block

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,78 +1,208 @@
-import project1 from '../images/project1.jpg';
-import project2 from '../images/project2.jpg';
-import project3 from '../images/project3.jpg';
-import project4 from '../images/project4.jpg';
-
 import './projects.css';
+import { ProjectsData } from './projectsData';
+import { useEffect, useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/grid';
+import 'swiper/css/pagination';
+import { Grid, Pagination } from 'swiper';
 
 export default function Projects() {
+    const webSites = useRef();
+    const brandIdentity = useRef();
+    const mobileApps = useRef();
+    const digitalProducts = useRef();
+
+    const webSitesButton = useRef();
+    const brandIdentityButton = useRef();
+    const mobileAppsButton = useRef();
+    const digitalProductsButton = useRef();
+    const projectsArr = [webSites, brandIdentity, mobileApps, digitalProducts];
+    const projectsBtnArr = [
+        webSitesButton,
+        brandIdentityButton,
+        mobileAppsButton,
+        digitalProductsButton,
+    ];
+
+    const [slidesPerView, setSlidesPerView] = useState(
+        window.innerWidth >= 1200 ? 2 : 1,
+    );
+    const [gridRows, setGridRows] = useState(window.innerWidth > 550 ? 2 : 1);
+
+    const checkDevice = () => {
+        if (window.innerWidth >= 1200) {
+            setSlidesPerView(2);
+            setGridRows(2);
+        } else if (window.innerWidth <= 1200) {
+            setSlidesPerView(1);
+            setGridRows(2);
+        }
+        if (window.innerWidth <= 550) {
+            setGridRows(1);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', checkDevice);
+        return () => {
+            window.removeEventListener('resize', checkDevice);
+        };
+    });
+
+    const onClick = (e, block) => {
+        projectsArr.forEach((el) => {
+            if (el === block) {
+                el.current.classList.remove('displayNone');
+            } else {
+                el.current.classList.add('displayNone');
+            }
+        });
+        projectsBtnArr.forEach((el) => el.current.classList.remove('active'));
+        e.target.classList.add('active');
+    };
+
     return (
         <div id="projects">
             <h1>case studies</h1>
             <div className="vrLine projectsMenu">
-                <p className="active">web sites</p>
-                <p>brand identity</p>
-                <p>mobile apps</p>
-                <p>digital products </p>
+                <p
+                    ref={webSitesButton}
+                    onClick={(e) => onClick(e, webSites)}
+                    className="active">
+                    web sites
+                </p>
+                <p
+                    ref={brandIdentityButton}
+                    onClick={(e) => onClick(e, brandIdentity)}
+                    className="">
+                    brand identity
+                </p>
+                <p
+                    ref={mobileAppsButton}
+                    onClick={(e) => onClick(e, mobileApps)}
+                    className="">
+                    mobile apps
+                </p>
+                <p
+                    ref={digitalProductsButton}
+                    onClick={(e) => onClick(e, digitalProducts)}
+                    className="">
+                    digital products
+                </p>
             </div>
 
-            <div id="webSitesBlock">
-                <div className="projectBlock">
-                    <img src={project1} alt="project1" />
-                    <div className="projectInfo">
-                        <p>full cycle creATION</p>
-                        <h4>Car rental service</h4>
-                        <p>
-                            Open repair of infrarenal aortic aneurysm or
-                            dissection, plus repair of associated arterial
-                            trauma, following unsuccessful endovascular repair;
-                            tube prosthesis.
-                        </p>
-                        <h3>View project</h3>
-                    </div>
-                </div>
-                <div className="projectBlock">
-                    <img src={project2} alt="project2" />
-                    <div className="projectInfo">
-                        <p>web design</p>
-                        <h4>Supply chain data platform</h4>
-                        <p>
-                            Open repair of infrarenal aortic aneurysm or
-                            dissection, plus repair of associated arterial
-                            trauma, following unsuccessful endovascular repair;
-                            tube prosthesis.
-                        </p>
-                        <h3>View project</h3>
-                    </div>
-                </div>
-                <div className="projectBlock">
-                    <div className="projectInfo">
-                        <p>admin system development</p>
-                        <h4>Streaming platform</h4>
-                        <p>
-                            Open repair of infrarenal aortic aneurysm or
-                            dissection, plus repair of associated arterial
-                            trauma, following unsuccessful endovascular repair;
-                            tube prosthesis.
-                        </p>
-                        <h3>View project</h3>
-                    </div>
-                    <img src={project3} alt="project3" />
-                </div>
-                <div className="projectBlock">
-                    <div className="projectInfo">
-                        <p>full cycle development</p>
-                        <h4>Fleet management system</h4>
-                        <p>
-                            Open repair of infrarenal aortic aneurysm or
-                            dissection, plus repair of associated arterial
-                            trauma, following unsuccessful endovascular repair;
-                            tube prosthesis.
-                        </p>
-                        <h3>View project</h3>
-                    </div>
-                    <img src={project4} alt="project4" />
-                </div>
+            <div id="webSites" ref={webSites} className="">
+                <Swiper
+                    slidesPerView={slidesPerView}
+                    grid={{
+                        rows: gridRows,
+                    }}
+                    spaceBetween={0}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Grid, Pagination]}
+                    className="mySwiper">
+                    {ProjectsData.webSites.map((project, i) => {
+                        return (
+                            <SwiperSlide key={i} className="projectBlock">
+                                <img src={project.imgSrc} alt="" />
+                                <div className="projectInfo">
+                                    <p>{project.projectInfo.preHeader}</p>
+                                    <h4>{project.projectInfo.header}</h4>
+                                    <p>{project.projectInfo.content}</p>
+                                    <h3>View project</h3>
+                                </div>
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
+            </div>
+
+            <div id="brandIdentity" ref={brandIdentity} className="displayNone">
+                <Swiper
+                    slidesPerView={slidesPerView}
+                    grid={{
+                        rows: gridRows,
+                    }}
+                    spaceBetween={0}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Grid, Pagination]}
+                    className="mySwiper">
+                    {ProjectsData.brandIdentity.map((project, i) => {
+                        return (
+                            <SwiperSlide key={i} className="projectBlock">
+                                <img src={project.imgSrc} alt="" />
+                                <div className="projectInfo">
+                                    <p>{project.projectInfo.preHeader}</p>
+                                    <h4>{project.projectInfo.header}</h4>
+                                    <p>{project.projectInfo.content}</p>
+                                    <h3>View project</h3>
+                                </div>
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
+            </div>
+            <div id="mobileApps" ref={mobileApps} className="displayNone">
+                <Swiper
+                    slidesPerView={slidesPerView}
+                    grid={{
+                        rows: gridRows,
+                    }}
+                    spaceBetween={0}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Grid, Pagination]}
+                    className="mySwiper">
+                    {ProjectsData.mobileApps.map((project, i) => {
+                        return (
+                            <SwiperSlide key={i} className="projectBlock">
+                                <img src={project.imgSrc} alt="" />
+                                <div className="projectInfo">
+                                    <p>{project.projectInfo.preHeader}</p>
+                                    <h4>{project.projectInfo.header}</h4>
+                                    <p>{project.projectInfo.content}</p>
+                                    <h3>View project</h3>
+                                </div>
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
+            </div>
+            <div
+                id="digitalProducts"
+                ref={digitalProducts}
+                className="displayNone">
+                <Swiper
+                    slidesPerView={slidesPerView}
+                    grid={{
+                        rows: gridRows,
+                    }}
+                    spaceBetween={0}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Grid, Pagination]}
+                    className="mySwiper">
+                    {ProjectsData.digitalProducts.map((project, i) => {
+                        return (
+                            <SwiperSlide key={i} className="projectBlock">
+                                <img src={project.imgSrc} alt="" />
+                                <div className="projectInfo">
+                                    <p>{project.projectInfo.preHeader}</p>
+                                    <h4>{project.projectInfo.header}</h4>
+                                    <p>{project.projectInfo.content}</p>
+                                    <h3>View project</h3>
+                                </div>
+                            </SwiperSlide>
+                        );
+                    })}
+                </Swiper>
             </div>
         </div>
     );
---
+++ src/components/projects/projectsData.js
@@ -0,0 +1,154 @@
+export const ProjectsData = {
+    webSites: [
+        {
+            imgSrc: require('../images/project1.jpg'),
+            projectInfo: {
+                preHeader: 'full cycle creATION',
+                header: 'Car rental service',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project2.jpg'),
+            projectInfo: {
+                preHeader: 'web design',
+                header: 'Supply chain data platform',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project3.jpg'),
+            projectInfo: {
+                preHeader: 'admin system development',
+                header: 'Streaming platform',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project4.jpg'),
+            projectInfo: {
+                preHeader: 'full cycle development',
+                header: 'Fleet management system',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+    ],
+    brandIdentity: [
+        {
+            imgSrc: require('../images/project2.jpg'),
+            projectInfo: {
+                preHeader: 'web design',
+                header: 'Supply chain data platform',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project3.jpg'),
+            projectInfo: {
+                preHeader: 'admin system development',
+                header: 'Streaming platform',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project4.jpg'),
+            projectInfo: {
+                preHeader: 'full cycle development',
+                header: 'Fleet management system',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project1.jpg'),
+            projectInfo: {
+                preHeader: 'full cycle creATION',
+                header: 'Car rental service',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+    ],
+    mobileApps: [
+        {
+            imgSrc: require('../images/project3.jpg'),
+            projectInfo: {
+                preHeader: 'admin system development',
+                header: 'Streaming platform',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project4.jpg'),
+            projectInfo: {
+                preHeader: 'full cycle development',
+                header: 'Fleet management system',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project1.jpg'),
+            projectInfo: {
+                preHeader: 'full cycle creATION',
+                header: 'Car rental service',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project2.jpg'),
+            projectInfo: {
+                preHeader: 'web design',
+                header: 'Supply chain data platform',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+    ],
+    digitalProducts: [
+        {
+            imgSrc: require('../images/project4.jpg'),
+            projectInfo: {
+                preHeader: 'full cycle development',
+                header: 'Fleet management system',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project1.jpg'),
+            projectInfo: {
+                preHeader: 'full cycle creATION',
+                header: 'Car rental service',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project2.jpg'),
+            projectInfo: {
+                preHeader: 'web design',
+                header: 'Supply chain data platform',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+        {
+            imgSrc: require('../images/project3.jpg'),
+            projectInfo: {
+                preHeader: 'admin system development',
+                header: 'Streaming platform',
+                content:
+                    'Open repair of infrarenal aortic aneurysm or dissection, plus repair of associated arterial trauma, following unsuccessful endovascular repair; tube prosthesis.',
+            },
+        },
+    ],
+};
